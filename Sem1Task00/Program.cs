3e96b5d390c164979d352a93b1cc1967fb7d3ca1
Ivanov Dmitry <id21@bk.ru>
2023-03-24 12:52:47 +0300

    нулевая задача решена

--- Sem1Task00/Program.cs
+++ Sem1Task00/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Просим ввести число
+Console.WriteLine("Введите число: ");
+// Считываем данные с консоли
+string? inLine = Console.ReadLine();
+// Проверяем, что данные не пустые
+if (inLine != null)
+{
+    // Парсим введенное число
+    int inNumber = int.Parse(inLine);
+    // Находим квадрат
+    int quadNumber = inNumber * inNumber;
+    // Выводим данные
+    Console.Write("Квадрат числа: ");
+    Console.WriteLine(quadNumber);
+
+}
