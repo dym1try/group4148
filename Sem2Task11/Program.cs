98138f2875a0313bcadde9323aaf23aa2b3709b8
Dmitry Ivanov <id21@bk.ru>
2023-03-30 19:18:33 +0300

    12-я задача второго семинара

--- Sem2Task11/Program.cs
+++ Sem2Task11/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// //Задача №11
+// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого числа. 
+int num = new Random().Next(100,1000);
+int fisNum = num/100;
+int lasNum = num%10;
+int newNum = fisNum*10 + lasNum;
+Console.WriteLine(num);
+Console.WriteLine(newNum);
