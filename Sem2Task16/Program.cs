83037fa3e8defbcfacb0323055eef7393994bcb4
Dmitry Ivanov <id21@bk.ru>
2023-03-30 19:40:33 +0300

    16-я задача второго семинара

---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №16
+// Напишите программу, которая принимает на вход
+// два числа и проверяет, является ли одно число квадратом другого. 
+
+Console.WriteLine("Введите первое число: ");
+// Более БЫСТРЫЙ метод проверки непустого ввода
+int num1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine() ?? "0");
+
+void SqrtTest(int a, int b)
+{
+    if (a == b * b)
+        Console.WriteLine(a + " это квадрат " + b);
+    else
+        Console.WriteLine(a + " это не квадрат " + b);
+}
+SqrtTest(num1, num2);
+SqrtTest(num2, num1);
