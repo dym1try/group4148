26f7dfa4d02d01e2b98f347b4683c7ea4bd61908
Dmitry Ivanov <id21@bk.ru>
2023-03-30 20:01:12 +0300

    16-я задача второго семинара 2

--- Sem2Task14/Program.cs
+++ Sem2Task14/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №14
+// Напишите программу, которая принимает на вход
+// число и проверяет, кратно ли оно одновременно 7 и 23. 
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()??"0");
+bool test1 = (num%7 == 0);
+bool test2 = (num%23 == 0);
+
+if(test1 && test2)
+{
+    Console.WriteLine("Число кратно и 7, и 23");
+}
+else
+{
+    Console.WriteLine("Число не кратно и 7, и 23");    
+}
