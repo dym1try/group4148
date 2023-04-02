96f723b38eda297ed8c87ad132001370017dc19f
Dmitry Ivanov <id21@bk.ru>
2023-04-02 21:26:48 +0300

    15-я задача второго семинара

--- Sem2Task15/Program.cs
+++ Sem2Task15/Program.cs
@@ -4,3 +4,24 @@
 // 7 -> да
 // 1 -> нет
 // * Сделать вариант с использованием конструкции Dictionary
+
+Dictionary<int, string> daysOfWeek = new Dictionary<int, string>();
+daysOfWeek.Add(1, "Будний день.");
+daysOfWeek.Add(2, "Будний день.");
+daysOfWeek.Add(3, "Будний день.");
+daysOfWeek.Add(4, "Будний день.");
+daysOfWeek.Add(5, "Будний день.");
+daysOfWeek.Add(6, "Выходной.");
+daysOfWeek.Add(7, "Выходной.");
+
+Console.WriteLine("Введите день недели (1-7): ");
+int day = int.Parse(Console.ReadLine()??"0");
+
+if (daysOfWeek.ContainsKey(day))
+{
+    Console.WriteLine(daysOfWeek[day]);
+}
+else
+{
+    Console.WriteLine("Это не день недели.");
+}
