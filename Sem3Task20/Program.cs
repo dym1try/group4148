e93d693b2731662b30c67bcce7875608949386f0
Ivanov Dmitry <id21@bk.ru>
2023-04-06 08:32:31 +0300

    Решение д.з. 21

--- Sem3Task20/Program.cs
+++ Sem3Task20/Program.cs
@@ -1,16 +1,22 @@
-﻿// Задача №20 // не готова
+﻿// Задача №20
 // Напишите программу, которая принимает 
 // на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
-int ReadData(string msg)
+int ReadData(string message)
 {
-    Console.WriteLine(msg);
+    Console.WriteLine(message);
     int res = int.Parse(Console.ReadLine() ?? "0");
     return res;
 }
-
+// метод принимает 4 переменных и возвращает одну
 double CalcLen2D(int x1, int y1, int x2, int y2)
 {
-    return Math.Sqrt(Math.Pow);
-
-
+    //     корень    возведение во 2 степ.   то же самое по простому
+    return Math.Sqrt(Math.Pow((x1 - x2), 2) + (y1-y2) * (y1-y2));
 }
+
+int x1 = ReadData("Введите координату X точки А: ");
+int y1 = ReadData("Введите координату Y точки А: ");
+int x2 = ReadData("Введите координату X точки B: ");
+int y2 = ReadData("Введите координату Y точки B: ");
+
+Console.WriteLine ("Расстояние между точками A и B: " + CalcLen2D(x1,y1,x2,y2));
---
+++ Sem3Task21/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача №21
+// Напишите программу, которая принимает 
+// на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    int res = int.Parse(Console.ReadLine() ?? "0");
+    return res;
+}
+// метод принимает 6 переменных и возвращает одну
+double CalcLen3D(int x1, int y1, int x2, int y2, int z1, int z2)
+{
+    //     корень    возведение во 2 степ.   
+    return Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2));
+}
+
+int x1 = ReadData("Введите координату X точки А: ");
+int y1 = ReadData("Введите координату Y точки А: ");
+int z1 = ReadData("Введите координату Z точки A: ");
+
+int x2 = ReadData("Введите координату X точки B: ");
+int y2 = ReadData("Введите координату Y точки B: ");
+int z2 = ReadData("Введите координату Z точки B: ");
+
+
+Console.WriteLine ("Расстояние между точками A и B: " + CalcLen3D(x1,y1,x2,y2,z1,z2));
