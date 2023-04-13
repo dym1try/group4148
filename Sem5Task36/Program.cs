6624ad13a498bb967be0f7c5b5f812c824a1875a
Ivanov Dmitry <id21@bk.ru>
2023-04-13 16:06:29 +0300

    Решение д.з. 13 апреля 2

---
+++ Sem5Task36/Program.cs
@@ -0,0 +1,51 @@
+﻿// №36 Задайте одномерный массив, заполненный случайными числами. Найдите сумму
+// элементов, стоящих на нечётных позициях. ODD 
+// [3, 7, 23, 12] -> 19
+// [3, 7, -2, 1] -> 8
+// [-4, -6, 89, 6] -> 0        * Найдите все пары в массиве и выведите пользователю
+
+// В конце 5 семинара Вы прочитали это Д\З, как сумму ЧЕТНЫХ позиций. В семинаре pdf и в ЛК написано про
+// НЕЧЕТНЫЕ... Решил сделать подсчет нечетных.
+
+//Ввод длины массива
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int res = int.Parse(Console.ReadLine() ?? "0");
+    return res;
+}
+//Метод печати одномерного массива
+void Print1Darray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ",");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+//Заполнение массива
+int[] Gen1DArray(int len, int top, int but)
+{
+    int[] res = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        res[i] = new Random().Next(but, top + 1);
+    }
+    return res;
+}
+
+int SumElmOddPos(int[] arr) // считаем сумму элементов, стоящих на нечётных позициях.
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i = i + 2)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+int lenArr = ReadData("Введите длину массива: ");
+int[] arr = Gen1DArray(lenArr, 999, 100);
+Print1Darray(arr); // выводим массив
+Console.WriteLine("Сумма элементов, стоящих на нечётных позициях: " + SumElmOddPos(arr));
---
+++ Sem5Task38/Program.cs
@@ -0,0 +1,57 @@
+﻿// №38 Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76         [2 0,4 9 7,2 78] -> 77,6
+// * Отсортируйте массив методом вставки и методом подсчета, а затем найдите
+// разницу между первым и последним элементом. Для задачи со звездочкой
+// использовать заполнение массива целыми числами.
+
+Console.Clear();
+
+//Ввод длины массива
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int res = int.Parse(Console.ReadLine() ?? "0");
+    return res;
+}
+//Метод печати одномерного массива
+void Print1Darray(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ",");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+//Заполнение массива ВЕЩЕСТВЕННЫМИ ЧИСЛАМИ
+double[] Gen1DArray(int len, int top, int but)
+{
+    double[] res = new double[len];
+    for (int i = 0; i < len; i++)
+    {
+        res[i] = new Random().NextDouble() * 200 - 100; // Задал диапазон от -100 до 100
+    }
+    return res;
+}
+
+double DiffMinMax(double[] arr) // Находим минимум, максимум и разницу между ними
+{
+    double res = 0;
+    double min = double.MaxValue;
+    double max = double.MinValue;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (min > arr[i]) min = arr[i];
+        if (max < arr[i]) max = arr[i];
+
+    }
+    res = max - min;
+    return res;
+}
+
+int lenArr = ReadData("Введите длину массива: ");
+double[] arr = Gen1DArray(lenArr, 999, 100);
+Print1Darray(arr); // выводим массив
+DiffMinMax(arr);
+Console.WriteLine("Разница между максимальным и минимальным элементами: " + DiffMinMax(arr));
